234963e7d5462b9bb36ae40a820ffd6001b84912
charles zanetti <charles.a.zanetti@gmail.com>
2023-11-02 14:30:30 -0300

    feat: learn with props

---
+++ my-app/src/components/Charles-CV.jsx
@@ -0,0 +1,11 @@
+ export const Charlescv = ({name}) => {
+
+    return (<div>
+
+    <h2>Teste CV {name}</h2>
+  
+   </div>)
+  
+
+}
+
--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import {Charlescv} from "../components/Charles-CV"
 
-const Home = () => {
+ export const Home = () => {
   return (
-   <div><
-    h1>Meu Cv React </h1>
+    <div>
+      <h1>Meu Cv React </h1>
+      <Charlescv name="charles"/>
+      <Charlescv name="Misael"/>
+      <Charlescv name="Wanderson"/>
+      <Charlescv name="Lucas"/>
+      <Charlescv name="Gilson"/>
     </div>
   );
 }
 
-export default Home;
+
